d78935ad70ed9e9981026f57d2ea45f1296db5aa
Francisco Guilherme <franciscoguilherme.mundo@outlook.com>
2022-05-30 12:22:37 -0300

    feat(order): :sparkles: implementa a criacao de uma nova order.

--- src/infrastructure/repository/order.repository.ts
+++ src/infrastructure/repository/order.repository.ts
@@ -1,9 +1,25 @@
 import Order from "../../domain/entity/order";
 import OrderInterface from "../../domain/repository/order-repository.interface";
+import OrderItemModel from "../db/sequelize/model/order-item.model";
+import OrderModel from "../db/sequelize/model/order.model";
 
 export default class OrderRepository implements OrderInterface {
   async create(entity: Order): Promise<void> {
-    throw new Error("");
+    await OrderModel.create({
+      id: entity.id,
+      customer_id: entity.customerId,
+      items: entity.items.map(item => ({
+        id: item.id,
+        name: item.name,
+        price: item.price,
+        product_id: item.productId,
+        quantity: item.quantity
+      })),
+      total: entity.total(),
+    },
+    {
+      include: [{ model: OrderItemModel }]
+    });
   }
 
   async update(entity: Order): Promise<void> {
