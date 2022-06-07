b2337e30187500cb980ef098e68430bc275610de
Francisco Guilherme <franciscoguilherme.mundo@outlook.com>
2022-06-07 18:28:06 -0300

    feat(event): :sparkles: implementa parcialmente o handler do evento de criacao do produto.

---
+++ src/event/product/handlers/send-email-when-product-is-created.handler.ts
@@ -0,0 +1,8 @@
+import HandlerInterface from '../../@shared/event-handler.interface';
+import ProductCreatedEvent from "../product-created.event";
+
+export default class SendEmailWhenProductIsCreatedHandler implements HandlerInterface<ProductCreatedEvent> {
+  handle(event: ProductCreatedEvent): void {
+
+  }
+}
