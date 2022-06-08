3ccfdc6affb8305518824c3442e964bb36a07655
LeandroSousaCastro <leandro.desousacastro@gmail.com>
2022-06-08 18:53:44 -0300

    refactor(product): :recycle: adiciona o console log para validacao do metodo no teste.

--- src/event/product/handlers/send-email-when-product-is-created.handler.ts
+++ src/event/product/handlers/send-email-when-product-is-created.handler.ts
@@ -1,8 +1,10 @@
-import HandlerInterface from '../../@shared/event-handler.interface';
+import HandlerInterface from "../../@shared/event-handler.interface";
 import ProductCreatedEvent from "../product-created.event";
 
-export default class SendEmailWhenProductIsCreatedHandler implements HandlerInterface<ProductCreatedEvent> {
+export default class SendEmailWhenProductIsCreatedHandler
+  implements HandlerInterface<ProductCreatedEvent>
+{
   handle(event: ProductCreatedEvent): void {
-
+    console.log("Sending e-mail...");
   }
 }
